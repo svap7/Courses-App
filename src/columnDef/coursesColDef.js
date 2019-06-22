15efc56c6dbc7eef8d00ad60dd580f68c839bd77
svapnilvraut <svapdfastest@gmail.com>
2019-06-22 22:54:17 +0530

    Manage and Add course

---
+++ src/columnDef/coursesColDef.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "reactstrap";
+
+export const columndef = [
+  {
+    Header: "Course Id",
+    accessor: "courseId",
+    show: false
+  },
+  {
+    Header: "Course Title",
+    accessor: "title"
+  },
+  {
+    id: "authorName",
+    Header: "Author Name",
+    accessor: auth => auth.author.name
+  },
+  {
+    Header: "Course Duration",
+    accessor: "duration"
+  },
+  {
+    Header: "Edit Course",
+    Cell: props => renderEditButton(props)
+  },
+  {
+    Header: "Delete Course",
+    cell: props => renderDeleteButton(props)
+  }
+];
+
+const renderEditButton = ({ row }) => {
+  return (
+    <Link to={{ pathname: `/course/${row.courseId}` }}>
+      <Button color="info">Edit Course</Button>
+    </Link>
+  );
+};
+
+const renderDeleteButton = ({ row }) => {
+  return <Button color="danger">Delete Course</Button>;
+};
